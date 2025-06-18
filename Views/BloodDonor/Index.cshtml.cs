a2f894aa446af8b51e4ce110c3d07837cdc02ab4
jahid-hridoy <jahidulislam453201@gmail.com>
2025-06-18 12:49:53 +0600

    Fixed Index error

--- Views/BloodDonor/Index.cshtml.cs
+++ Views/BloodDonor/Index.cshtml.cs
@@ -1,29 +1,11 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.EntityFrameworkCore;
-using BloodDonorProject.Data;
-using BloodDonorProject.Models;
 
-namespace BloodDonorProject.Views.BloodDonor
+namespace BloodDonorProject.Views.BloodDonor;
+
+public class Index : PageModel
 {
-    public class IndexModel : PageModel
+    public void OnGet()
     {
-        private readonly BloodDonorProject.Data.BloodDonorDbContext _context;
-
-        public IndexModel(BloodDonorProject.Data.BloodDonorDbContext context)
-        {
-            _context = context;
-        }
-
-        public IList<Models.BloodDonor> BloodDonor { get;set; } = default!;
-
-        public async Task OnGetAsync()
-        {
-            BloodDonor = await _context.BloodDonors.ToListAsync();
-        }
+        
     }
 }
